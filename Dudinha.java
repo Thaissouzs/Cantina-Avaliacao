a40b66a1e1a172c8eabc9d89892f64ed7125e282
Thaissouzs <105057093+Thaissouzs@users.noreply.github.com>
2022-06-03 14:15:48 -0300

    teste de upload
    ASDFSD

---
+++ Dudinha.java
@@ -0,0 +1,19 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class Dudinha {
+
+    @FXML
+    private Button segunda;
+
+    @FXML
+    private TableView<?> tabela;
+
+    @FXML
+    void mostrarCardapioSegunda(ActionEvent event) {
+
+    }
+
+}
