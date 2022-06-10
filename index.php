1a3062dde0813505d05bca404b703f577e6e8e0b
Beamaial <106749431+Beamaial@users.noreply.github.com>
2022-06-10 16:29:55 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Refeitorio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="btn">
+        <button id="card"> Cardápio </button>
+        <button id="rod"> Rodizío </button>
+        <button id="ava"> Avaliação</button>
+        <button id="rest"> Restrições </button>
+    </div>
+</body>
+</html>
