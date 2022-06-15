f9fd9be38495059fc1a6b206b625691e462fec97
Duda1080 <107064728+Duda1080@users.noreply.github.com>
2022-06-15 13:55:32 -0300

    Add files via upload

--- Rodizio.java
+++ Rodizio.java
@@ -1,22 +1,51 @@
 package visual;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import model.Salas;
 
-public class Rodizio {
+public class Rodizio implements Initializable {
 
     private Parent root;
     private Stage tela;
-    private Scene cena;   
+    private Scene cena; 
+
+
+     @FXML
+    private Button btnvoltar;
 
     @FXML
-    private Button voltarMenu;
+    private TableColumn<Salas, String> quarta;
+
+    @FXML
+    private TableColumn<Salas, String> quinta;
+
+    @FXML
+    private TableColumn<Salas, String> segunda;
+
+    @FXML
+    private TableColumn<Salas, String> sexta;
+
+    @FXML
+    private TableColumn<Salas, String> terca;
+
+    @FXML
+    private TableView<Salas> tabela;    
 
     private void abrirTela(ActionEvent evento, String arquivo, String css) throws Exception{
         // Coloca arquivo na memória para carregar
@@ -32,8 +61,24 @@ public class Rodizio {
         tela.show();
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+       segunda.setCellValueFactory(new PropertyValueFactory<>("segunda"));
+       terca.setCellValueFactory(new PropertyValueFactory<>("terca"));
+       quarta.setCellValueFactory(new PropertyValueFactory<>("quarta"));
+       quinta.setCellValueFactory(new PropertyValueFactory<>("quinta"));
+       sexta.setCellValueFactory(new PropertyValueFactory<>("sexta"));
+       tabela.setItems(criarLista());
+    }
+    private ObservableList<Salas> criarLista(){
+        return FXCollections.observableArrayList(
+            new Salas("1 infor", "", "", "", "" )
+            
+        );
+    }
+
     @FXML
-    void VoltarParaMenu(ActionEvent event) throws Exception{
+    void voltarMenu(ActionEvent event) throws Exception{
 
         abrirTela(event, "Menu.fxml", "Fundo.css");
 
