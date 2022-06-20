3aecb7d78991d3b5ac1b5e931e853312cd50d04f
criis01 <107065730+criis01@users.noreply.github.com>
2022-06-20 18:18:19 -0300

    Add files via upload

---
+++ app/index.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Refeitorio</title>
+    <link rel="stylesheet" href="visual/inicio.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+</head>
+<body>
+  <div class="container">
+
+    <div class="menu">
+<div class="btnCardapio">
+  <a id="cardapio">Cardapio</a>
+  </div>
+  <div class="btnRozidio">
+  <a id="rodizio">Rodizio</a>
+  </div>
+  <div class="btnAvaliacao">
+  <a id="avaliacao">Avaliação</a>
+  </div>
+  <div class="btnRestricoes">
+  <a id="restricoes">Restrições</a>
+</div>
+    </div>
+<div class="imagem">
+<img src="visual/Logoo.png">
+</div>
+</div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
