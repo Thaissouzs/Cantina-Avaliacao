d8fac13cb5cb39a3ea44713b7a86582e1ac7595e
Duda1080 <107064728+Duda1080@users.noreply.github.com>
2022-06-20 18:03:45 -0300

    Add files via upload

---
+++ app/cardapio.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rodizio</title>
+    <link rel="stylesheet" href="visual/stylee.css">
+</head>
+<body>
+
+<div class="menu">
+    
+<div class="btnCardapio">
+  <a id="cardapio">Cardapio</a>
+  </div>
+  <div class="btnRozidio">
+  <a id="rodizio">Rodizio</a>
+  </div>
+  <div class="btnAvaliacao">
+  <a id="avaliacao">Avaliação</a>
+  </div>
+  <div class="btnRestricoes">
+  <a id="restricoes">Restrições</a>
+</div>
+    </div>
+<div class="tela" style="padding-bottom:160px; padding-top: 50px">
+    <br>
+    <h1>Cardapio Semanal</h1>
+    <p>Este é o cardapio da semana.</p>
+    <table border="1" align-text="center-left" id="tabelinha" style="padding-top: 0px;margin-left: px;margin-right: 420px;border-top-style: solid;border-top-width: 10px;border-left-width: 10px;border-left-style: solid;border-right-width: 10px;border-right-style: solid;border-bottom-width: 10px;border-bottom-style: solid;margin-top: 50px; width: 100%; height:50%">
+    <tr>
+        
+        <th>Dia da semana</th>
+        <th>Lanche Manha</th>
+        <th>Almoco</th>
+        <th>Lanche Tarde</th>
+
+    </tr>
+    <tr>
+        <td>Segunda</td>
+        <td>Caldo de Carne com torradas</td>
+        <td>Fricassê de frango, Arroz, Farofa e Salada(Repolho e Cenoura)</td>
+        <td>Café com Leite e <br>Biscoito </td>
+    </tr>
+    <tr>
+        <td>Terça</td>
+        <td>Suco de Fruta e Cuscuz com Frango</td>
+        <td>Estrogonofe de canre, Arroz, Feijao, Salada(Alface, Repolho e Maçã) e Melancia</td>
+        <td>Suco de fruta e <br> Pão com margarina</td>
+    </tr>
+    <tr>
+        <td>Quarta</td>
+        <td>Café com leite e Pão doce</td>
+        <td>Isca de Frango, Baião, purê, salada cozida(Batata doce, Beterraba e milho verde)</td>
+        <td>Café com leite, Cuscuz com carne e Melão</td>
+    </tr>
+    <tr>
+        <td>Quinta</td>
+        <td>Suco de Fruta e Cuscuz com Ovos</td>
+        <td>Picadinho de Carne a Primavera, Arroz, Feijão, Salada crua(Alface, Pepino e Melão) e Mamão</td>
+        <td>Suco de Fruta com Bolo</td>
+    </tr>
+    <tr>
+        <td>Sexta</td>
+        <td>Café com Leite e Pão com Pate de Frango</td>
+        <td>Feijoada, Arroz, Farofa, Salada crua(Alface, tomate, Laranja) e Suco de Fruta</td>
+        <td>Vitamina de Fruta com Biscoito</td>
+    </tr>
+    </table>
+</div>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="js/script.js"></script>
+
+</body>
+</html>
+
+
+
