00e0b733e0562896cc4d95e936d33f8c01e63ce8
Beamaial <106749431+Beamaial@users.noreply.github.com>
2022-06-20 18:10:46 -0300

    Add files via upload

---
+++ app/rodizio.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rodizio</title>
+    <link rel="stylesheet" href="visual/stylee.css">
+</head>
+<body>
+
+<div class="menu">
+    
+<div class="btnCardapio">
+  <a id="cardapio">Cardapio</a>
+  </div>
+  <div class="btnRozidio">
+  <a id="rodizio">Rodizio</a>
+  </div>
+  <div class="btnAvaliacao">
+  <a id="avaliacao">Avaliação</a>
+  </div>
+  <div class="btnRestricoes">
+  <a id="restricoes">Restrições</a>
+</div>
+    </div>
+<div class="tela" style="padding-bottom: 170px; padding-top: 50px">
+    <br>
+    <h1>Rodizio das salas</h1>
+    <p>Esta é a ordem de saida das salas para o almoço</p>
+    <table border="1" align="center-left" id="tabelinha" style="padding-top: 0px;margin-left: px;margin-right: 420px;border-top-style: solid;border-top-width: 10px;border-left-width: 10px;border-left-style: solid;border-right-width: 10px;border-right-style: solid;border-bottom-width: 10px;border-bottom-style: solid;margin-top: 50px; width: 100%; height:50%">
+    <tr>
+        <th>Segunda</th>
+        <th>Terça</th>
+        <th> Quarta</th>
+        <th> Quinta </th>
+        <th> Sexta </th>
+    </tr>
+    <tr>
+        <td>1 Guia</td>
+        <td>2 Informatica</td>
+        <td>3 Enfermagem</td>
+        <td>1 Informatica</td>
+        <td>2 Guia</td>
+    </tr>
+    <tr>
+        <td>1 Informatica</td>
+        <td>2 Enfermagem</td>
+        <td>3 Guia</td>
+        <td>1 Enfermagem</td>
+        <td>2 Informatica</td>
+    </tr>
+    <tr>
+        <td>1 Enfermagem</td>
+        <td>2 Guia</td>
+        <td>3 Informatica</td>
+        <td>1 Guia</td>
+        <td>2 Enfermagem</td>
+    </tr>
+    <tr>
+        <td>3 Informatica</td>
+        <td>1 Enfermagem</td>
+        <td>2 Guia</td>
+        <td>3 Enfermagem</td>
+        <td>1 Informatica</td>
+    </tr>
+    <tr>
+        <td>3 Enfermagem</td>
+        <td>1 Guia</td>
+        <td>2 Informatica</td>
+        <td>3 Guia</td>
+        <td>1 Enfermagem</td>
+    </tr>
+    <tr>
+        <td>3 Guia</td>
+        <td>1 Informatica</td>
+        <td>2 Enfermagem</td>
+        <td>3 Informatica</td>
+        <td>1 Guia</td>
+    </tr>
+    <tr>
+        <td>2 Enfermagem</td>
+        <td>3 Guia</td>
+        <td>1 Informatica</td>
+        <td>2 Guia</td>
+        <td>3 Enfermagem</td>
+    </tr>
+    <tr>
+        <td>2 Guia</td>
+        <td>3 Informatica</td>
+        <td>1 Enfermagem</td>
+        <td>2 Informatica</td>
+        <td>3 Guia</td>
+    </tr>
+    <tr>
+        <td>2 Informatica</td>
+        <td>3 Enfermagem</td>
+        <td>1 Guia</td>
+        <td>2 Enfermagem</td>
+        <td>3 Informatica</td>
+    </tr>
+    </table>
+</div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+
+</body>
+</html>
+
+
+
